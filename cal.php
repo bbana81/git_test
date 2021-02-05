f1480931d737ad3f3f096d58571b42ab98ed18d6
jang-han <jang-han@threet.co.jp>
2021-02-05 23:02:48 +0900

    cal_jap

--- cal.php
+++ cal.php
@@ -2,5 +2,5 @@
 $now = new date();
 $year = $now -> format('Y');
 $month = $now -> format('m');
-echo "this year is ".$year." and this month is ".$month.".";
+echo "今年は".$year." そして今月は ".$month."です.";
 ?>
